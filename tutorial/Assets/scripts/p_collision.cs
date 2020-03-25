ac390dcc9dfaa4c40a52504fe07605d5c5369240
technoe <technoe@gmail.com>
2020-03-24 22:41:54 -0500

    scoreboard, new fonts, and extra scripts

---
+++ tutorial/Assets/scripts/p_collision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p_collision : MonoBehaviour
+{
+    public p_movement movement;
+    void OnCollisionEnter (Collision collisionInfo) 
+    {
+        if (collisionInfo.collider.tag == "col_obstacle")
+        {
+            movement.enabled = false;
+            Debug.Log("movement disabled");
+        }        
+    }
+}
---
+++ tutorial/Assets/scripts/scoreboard.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreboard : MonoBehaviour
+{    
+    public Transform player;
+    public Text score;
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(player.position.z.ToString("0"));
+        score.text = player.position.z.ToString("0");
+    }
+}
